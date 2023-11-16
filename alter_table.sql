730f323afec7cc5eb2c4fc94efcd3756b4f5ca9b
DonAllenII <don.a.alexanderii@gmail.com>
2023-11-16 18:33:56 -0500

    updated alter_tables file

--- alter_table.sql
+++ alter_table.sql
@@ -6,18 +6,9 @@ alter table invoice
 	drop column sales_id,
 	add column sales_id smallserial references salesperson(sales_id);
 	
-alter table service_ticket
-	alter column ticket_id type smallint,
-	add column vin smallint references cars(vin),
-	add column part_id smallint references parts(part_id),
-	add column mechanic_id smallint references mechanics(mechanic_id);
 	
 alter table service_ticket 
 	drop column part_id,
 	add column part_id  integer [],
 	drop column mechanic_id,
 	add column mechanic_id  integer [];
-
-
-
-);
